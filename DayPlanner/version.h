acfd8179086f26ce7ad2f27cd86f289ed5c44e9f
Philp Masser <philip.masser@gmail.com>
2017-06-24 18:26:13 +0200

    Set version to 0.5.1

--- DayPlanner/version.h
+++ DayPlanner/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             0,5,0,0
-#define VER_FILEVERSION_STR         "0.5.0.0\0"
+#define VER_FILEVERSION             0,5,1,0
+#define VER_FILEVERSION_STR         "0.5.1.0\0"
 
-#define VER_PRODUCTVERSION          0,5,0,0
-#define VER_PRODUCTVERSION_STR      "0.5\0"
+#define VER_PRODUCTVERSION          0,5,1,0
+#define VER_PRODUCTVERSION_STR      "0.5.1\0"
 
 #define VER_COMPANYNAME_STR         "King Nak"
 #define VER_FILEDESCRIPTION_STR     "DayPlanner"
